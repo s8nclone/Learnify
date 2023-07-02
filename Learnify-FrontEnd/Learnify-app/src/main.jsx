d00d6cabdcd3745ee28a3bc2da5343b2a56e9523
Dayo Farayola <dayojnr.3@gmail.com>
2023-07-02 02:58:53 +0100

    adding pages and routes

--- Learnify-FrontEnd/Learnify-app/src/main.jsx
+++ Learnify-FrontEnd/Learnify-app/src/main.jsx
@@ -2,9 +2,57 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import ErrorPage from './pages/ErrorPage.jsx'
+import HomePage from './pages/HomePage.jsx'
+import CoursesPage from './pages/CoursesPage.jsx'
+import BlogPage from './pages/BlogPage.jsx'
+import AboutUsPage from './pages/AboutUsPage.jsx'
+import MainPage from './pages/MainPage.jsx'
+import SignUpPage from './pages/SignUpPage.jsx'
+import LoginPage from './pages/LoginPage.jsx'
+
+const route = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "/",
+        element: <MainPage />
+      },
+      {
+        path: "Home",
+        element: <HomePage />
+      },
+      {
+        path: "Courses",
+        element: <CoursesPage />
+      },
+      {
+        path: "Blog",
+        element: <BlogPage />
+      },
+      {
+        path: "AboutUs",
+        element: <AboutUsPage />
+      },
+      {
+        path: "SignUp",
+        element: <SignUpPage />
+      },
+      {
+        path: "Login",
+        element: <LoginPage />
+      },
+    ]
+  }
+])
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={route}/>
   </React.StrictMode>,
 )
