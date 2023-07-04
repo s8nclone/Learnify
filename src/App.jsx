76d34672a45d333fce8d26e2ad41c3e04e5253ba
AKPOCHI <akpochipaul@gmail.com>
2023-07-04 08:54:56 -0700

    committing navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from 'react'
 import './App.css'
 import { Outlet } from 'react-router-dom'
+import Navbar from './Components/Header/Navbar'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+      <Navbar/>
       <Outlet />
       
     </>
