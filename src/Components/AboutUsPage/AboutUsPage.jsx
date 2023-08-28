818ab732f7f4aa85f40e7b0b271da8b954a564fc
AKPOCHI <akpochipaul@gmail.com>
2023-08-28 16:50:32 -0700

    committing develop changes

---
+++ src/Components/AboutUsPage/AboutUsPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './AboutUsPage.css'
+function AboutUsPage(){
+  return(
+    <>
+    <div>
+      <p>ddjjdj</p>
+    </div>
+    </>
+  )
+}
+export default AboutUsPage;
---
+++ src/pages/AboutUsPage.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import AboutUsPage from '../Components/AboutUsPage/AboutUsPage'
+function AboutUsPage(){
+  return(
+    <AboutUsPage/>
+  )
+}
+export default AboutUsPage;
