aba42424ed6cd404e833a274f8867a2065e79098
Dayo Farayola <dayojnr.3@gmail.com>
2023-11-01 15:50:48 +0100

    fix reset password progress bar

--- src/components/Login/ForgotPassword/ForgotPasswordPath.jsx
+++ src/components/Login/ForgotPassword/ForgotPasswordPath.jsx
@@ -2,28 +2,25 @@ import React from 'react'
 import forgotPassLine from '../../../assets/forgotPasswordLine.svg'
 import { useLocation } from 'react-router-dom';
 import { AiOutlineCheck } from 'react-icons/ai'
-import { useContext } from 'react';
-import { AuthContext } from '../../../hooks/context';
 
 function ForgotPasswordPath() {
-  const { passwordProgress } = useContext(AuthContext);
 
   const location = useLocation();
 
-  const isTwoOpen = location.pathname === "/login/forgotpassword/2"
+  const isTwoOpen = location.pathname >= "/login/forgotpassword/2"
 
-  const isThreeOpen = location.pathname === "/login/forgotpassword/3"
+  const isThreeOpen = location.pathname >= "/login/forgotpassword/3"
 
-  const isFourOpen = location.pathname === "/login/forgotpassword/complete"
+  const isFourOpen = location.pathname >= "/login/forgotpassword/complete"
 
   return (
     <>
         <div className="forgot-pass-progress">
             <div className="forgot-pass-milestone pass-progress">1</div>
-            <div className="forgot-pass-line"><img src={forgotPassLine} className={(passwordProgress && isTwoOpen) ? "pass-progress-bar" : null}/></div>
-            <div className={`forgot-pass-milestone ${(passwordProgress && isTwoOpen) ? "pass-progress" : null }`}>2</div>
-            <div className="forgot-pass-line"><img src={forgotPassLine} className={(passwordProgress && isThreeOpen) ? "pass-progress-bar" : null}/></div>
-            <div className={`forgot-pass-milestone ${(passwordProgress && isThreeOpen) ? "pass-progress" : null }`}>3</div>
+            <div className="forgot-pass-line"><img src={forgotPassLine} className={ isTwoOpen ? "pass-progress-bar" : null}/></div>
+            <div className={`forgot-pass-milestone ${ isTwoOpen ? "pass-progress" : null }`}>2</div>
+            <div className="forgot-pass-line"><img src={forgotPassLine} className={ isThreeOpen ? "pass-progress-bar" : null}/></div>
+            <div className={`forgot-pass-milestone ${ isThreeOpen ? "pass-progress" : null }`}>3</div>
             <div className="forgot-pass-line"><img src={forgotPassLine} className={isFourOpen ? "pass-progress-bar" : null}/></div>
             <div className={`forgot-pass-milestone ${isFourOpen ? "pass-progress" : null }`}>{isFourOpen ? <AiOutlineCheck /> : 4}</div>
         </div>
