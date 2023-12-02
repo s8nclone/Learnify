83e9a864afe54a3958f971cbf1127a71cfce4060
Dayo Farayola <dayojnr.3@gmail.com>
2023-12-02 03:38:06 +0100

    adding changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
-import { useState } from 'react'
 import './App.css'
 import { Outlet } from 'react-router-dom'
 import Navbar from './Components/Header/Navbar'
 import Footer from './Components/FooterSection/Footer'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
